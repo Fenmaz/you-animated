d640641710d6face962d9db75f0f980c52464d49
Mark Coretsopoulos <mcoretso@macalester.edu>
2019-03-03 13:28:50 -0600

    added a variety of models in the resources folder, hopefully one of them can be useful for testing

--- src/App.cpp
+++ src/App.cpp
@@ -80,7 +80,7 @@ void App::onRenderGraphicsContext(const VRGraphicsState &renderState){
         reloadShaders();
         
         //import a new model to use in the program
-        _modelMesh.reset(new AnimatedModel("free3Dmodel.dae", 1.0, vec4(1.0)));
+        _modelMesh.reset(new AnimatedModel("Mettaur_BN.dae", 1.0, vec4(1.0)));
     }
 }
 
@@ -116,7 +116,7 @@ void App::onRenderGraphicsScene(const VRGraphicsState &renderState){
     float time = (float) (VRSystem::getTime() - _startTime);
     vector<glm::mat4> transforms;
     
-    _modelMesh->boneTransform(time, transforms);
+    //_modelMesh->boneTransform(time, transforms);
     //printf("%f\n", time);
     
     // Draw the model
