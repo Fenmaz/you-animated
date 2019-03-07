309e2b4cd97573a84e0fedbfd52ee012ef9ae1e3
Fenmaz <tnguyen9@macalester.edu>
2019-03-07 17:22:49 -0600

    comment out unused time variable and change model

--- src/App.cpp
+++ src/App.cpp
@@ -80,7 +80,7 @@ void App::onRenderGraphicsContext(const VRGraphicsState &renderState){
         reloadShaders();
         
         //import a new model to use in the program
-        _modelMesh.reset(new AnimatedModel("Mettaur_BN.dae", 1.0, vec4(1.0)));
+        _modelMesh.reset(new AnimatedModel("Cigarette_Box_-_Copy.dae", 1.0, vec4(1.0)));
     }
 }
 
@@ -113,7 +113,7 @@ void App::onRenderGraphicsScene(const VRGraphicsState &renderState){
     _shader.setUniform("normal_mat", mat3(transpose(inverse(model))));
     _shader.setUniform("eye_world", eye_world);
     
-    float time = (float) (VRSystem::getTime() - _startTime);
+    //float time = (float) (VRSystem::getTime() - _startTime);
     vector<glm::mat4> transforms;
     
     //_modelMesh->boneTransform(time, transforms);
