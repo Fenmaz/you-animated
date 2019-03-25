d6c09c8125a91df47897e5c3502d53dd4b3a05b4
Mark Coretsopoulos <mcoretso@macalester.edu>
2019-03-25 17:00:39 -0500

    ANIMATIONS WORKING

--- src/App.cpp
+++ src/App.cpp
@@ -80,7 +80,7 @@ void App::onRenderGraphicsContext(const VRGraphicsState &renderState){
         reloadShaders();
         
         //import a new model to use in the program
-        _modelMesh.reset(new AnimatedModel("ArmatureForkedComplex.dae", 1.0, vec4(1.0)));
+        _modelMesh.reset(new AnimatedModel("roller shutters3.dae", 1.0, vec4(1.0)));
     }
 }
 
@@ -113,10 +113,10 @@ void App::onRenderGraphicsScene(const VRGraphicsState &renderState){
     _shader.setUniform("normal_mat", mat3(transpose(inverse(model))));
     _shader.setUniform("eye_world", eye_world);
     
-    //float time = (float) (VRSystem::getTime() - _startTime);
+    float time = (float) (VRSystem::getTime() - _startTime);
     vector<glm::mat4> transforms;
     
-    //_modelMesh->boneTransform(time, transforms);
+    _modelMesh->boneTransform(time, transforms);
     //printf("%f\n", time);
     
     // Draw the model
