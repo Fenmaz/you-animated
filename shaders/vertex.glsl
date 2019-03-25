126c6c73a9012d495efa9ba0c29aab71a12b3692
Fenmaz <tnguyen9@macalester.edu>
2019-03-25 17:01:56 -0500

    delete incorrect comments

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,8 +3,8 @@
 layout (location = 0) in vec3 vertex_position;
 layout (location = 1) in vec3 vertex_normal;
 layout (location = 2) in vec2 vertex_texcoord;
-layout (location = 3) in ivec4 boneIDs; // This is probably wrong
-layout (location = 4) in vec4 weights; // This is probably wrong
+layout (location = 3) in ivec4 boneIDs;
+layout (location = 4) in vec4 weights;
 
 uniform mat4 projection_mat, view_mat, model_mat;
 uniform mat3 normal_mat;
